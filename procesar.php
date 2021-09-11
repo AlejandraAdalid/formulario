ea80c2ccdc3b09f308d4a73ccd8e52e9ed36bdec
PatricioStraziuso <straziuso.patricio@gmail.com>
2021-09-10 21:14:58 -0300

    agregamos formulaio y la seccion faltante

--- procesar.php
+++ procesar.php
@@ -1,10 +1,12 @@
 <?php
 $nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
 $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
  
 echo "<h2>Informacion recibida desde PHP</h2>";
 echo "El nombre recibido es: " . $nombre . "<br/>";
+echo "El apellido recibido es: " . $apellido . "<br/>";
 echo "El asunto recibido es: " . $asunto . "<br/>";
 echo "El mensaje recibido es: " . $mensaje . "<br/>";
  
